5a4fc7a24f88c0fcbad235ab6cc83da64e02e0ce
Nam <22130173@st.hcmuaf.edu.vn>
2025-09-12 13:09:13 +0700

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import './index.css';
 import Header from './components/Header.jsx';
+import Banner from './components/Banner.jsx';
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gray-100">
+      <div>
         <Header />
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="text-4xl text-center text-blue-600 mb-8">
-            Welcome to Movie App
-          </h1>
-          <p className="text-lg text-gray-700 text-center">
-            Tailwind CSS đã được cài đặt thành công!
-          </p>
-        </div>
+        <Banner />
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,90 @@
+import React from 'react';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div
+      style={{ backgroundColor: 'black' }}
+      className="header p-4 bg-black text-white flex items-center justify-between"
+    >
+      <div className="text-2xl font-bold space-x-4 flex items-center">
+        <h1
+          className=" uppercase font-bold text-red-500"
+          style={{ color: 'red' }}
+        >
+          Movie
+        </h1>
+        <nav>
+          <ul
+            style={{ padding: '1rem', color: 'white' }}
+            className="flex space-x-4 text-decoration-none list-none "
+          >
+            <li>
+              <a
+                href="#"
+                style={{
+                  padding: '1rem',
+                  color: 'white',
+                  textDecoration: 'none',
+                }}
+                className="hover:text-red-500 "
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{
+                  padding: '1rem',
+                  color: 'white',
+                  textDecoration: 'none',
+                }}
+                className="hover:text-red-500 ml-4"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                style={{
+                  padding: '1rem',
+                  color: 'white',
+                  textDecoration: 'none',
+                }}
+                className="hover:text-red-500"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <input
+          style={{
+            border: 'none',
+            height: '32px',
+          }}
+          type="text"
+          placeholder="Search..."
+          className="px-2 py-1 rounded bg-black-700 border border-gray-600 text-white"
+        />
+        <button
+          style={{
+            backgroundColor: 'red',
+            color: 'white',
+            border: 'none',
+            height: '32px',
+            margin: '0 16px',
+          }}
+          className="ml-2 px-4 py-1 rounded bg-red-500 hover:bg-red-600"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
